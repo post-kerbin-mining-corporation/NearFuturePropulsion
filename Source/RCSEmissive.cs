bc29ba217c11dfb174821254b7f418ccc12ab01b
Chris <cadderley@gmail.com>
2020-12-22 08:04:23 -0800

    Prepare 1.3.0

--- Source/RCSEmissive.cs
+++ Source/RCSEmissive.cs
@@ -11,53 +11,54 @@ using UnityEngine;
 
 namespace NearFuturePropulsion
 {
-    public class ModuleRCSEmissive : PartModule
+  public class ModuleRCSEmissive : PartModule
+  {
+    [KSPField(isPersistant = false)]
+    public FloatCurve alphaCurve = new FloatCurve();
+
+    [KSPField(isPersistant = false)]
+    public FloatCurve blueCurve = new FloatCurve();
+
+    [KSPField(isPersistant = false)]
+    public FloatCurve greenCurve = new FloatCurve();
+
+    [KSPField(isPersistant = false)]
+    public FloatCurve redCurve = new FloatCurve();
+
+    [KSPField(isPersistant = false)]
+    public string shaderColorParameter = "_EmissiveColor";
+
+    ModuleRCSFX rcs;
+    List<Material> thrustMaterials;
+
+    public void Start()
     {
-        [KSPField(isPersistant = false)]
-        public FloatCurve alphaCurve = new FloatCurve();
+      rcs = part.GetComponent<ModuleRCSFX>();
 
-        [KSPField(isPersistant = false)]
-        public FloatCurve blueCurve = new FloatCurve();
-
-        [KSPField(isPersistant = false)]
-        public FloatCurve greenCurve = new FloatCurve();
-
-        [KSPField(isPersistant = false)]
-        public FloatCurve redCurve = new FloatCurve();
-
-        [KSPField(isPersistant = false)]
-        public string shaderColorParameter = "_EmissiveColor";
-
-        ModuleRCSFX rcs;
-        List<Material> thrustMaterials;
-
-        public void Start()
-        {
-            rcs = part.GetComponent<ModuleRCSFX>();
-
-            thrustMaterials = new List<Material>();
-            foreach (Transform t in rcs.thrusterTransforms)
-            {
-                thrustMaterials.Add(t.GetComponentInChildren<MeshRenderer>().material);
-            }
-        }
-
-        public void FixedUpdate()
-        {
-
-            if (HighLogic.LoadedSceneIsFlight)
-            {
-                for (int i = 0; i < thrustMaterials.Count; i++)
-                {
-                    Color c;
-                    c = new Color(redCurve.Evaluate(rcs.thrustForces[i]),
-                                  greenCurve.Evaluate(rcs.thrustForces[i]),
-                                  blueCurve.Evaluate(rcs.thrustForces[i]),
-                                  alphaCurve.Evaluate(rcs.thrustForces[i]));
-                    thrustMaterials[i].SetColor(shaderColorParameter, c);
-
-                }
-            }
-        }
+      thrustMaterials = new List<Material>();
+      foreach (Transform t in rcs.thrusterTransforms)
+      {
+        thrustMaterials.Add(t.GetComponentInChildren<MeshRenderer>().material);
+      }
     }
+
+    public void FixedUpdate()
+    {
+
+      if (HighLogic.LoadedSceneIsFlight)
+      {
+        for (int i = 0; i < thrustMaterials.Count; i++)
+        {
+          float thrust = rcs.thrustForces[i] / rcs.thrusterPower;
+                    Color c;
+          c = new Color(redCurve.Evaluate(thrust),
+                        greenCurve.Evaluate(thrust),
+                        blueCurve.Evaluate(thrust),
+                        alphaCurve.Evaluate(thrust));
+          thrustMaterials[i].SetColor(shaderColorParameter, c);
+
+        }
+      }
+    }
+  }
 }
